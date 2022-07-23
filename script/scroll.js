dab8b0f3caf130b27659d31434684bc9493e06c7
Jhonathan <ajhonathan73@gmail.com>
2022-07-23 15:30:12 -0300

    Adicionando scrollreaveal na landing page

---
+++ script/scroll.js
@@ -0,0 +1,10 @@
+// SCROOL REVEAL
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+}).reveal(`#header, 
+            #hero-main,
+            #blog,
+            #about,
+            #footer`);
